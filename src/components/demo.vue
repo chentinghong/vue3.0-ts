553a00829251419bf6af062c96a4e32f5df3bdda
thchen <thchen@hansyinfo.com>
2021-04-12 18:06:15 +0800

    测试v-model

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -16,28 +16,33 @@
         </template>
     </demo-child>
     <demo-child>
-        <template v-slot:foot="msg">
-            <div>
-                {{msg.footerMsg}}
-            </div>
-        </template>
-    </demo-child>
-    <demo-child>
         <template #foot="msg">
             <div>
                 {{msg.footerMsg}}
             </div>
         </template>
     </demo-child>
+    <!-- 等效写法
+    <demo-child>
+        <template v-slot:foot="msg">
+            <div>
+                {{msg.footerMsg}}
+            </div>
+        </template>
+    </demo-child> -->
+    
     <br>
     <p>测试provide</p>
     <demo-child :msg="{msg:'测试传入的参数'}" class="demo" />
     <button @click="changeProvideData">改变组件中的传递值</button>
+    <p>父子组件之间v-model的运用</p>
+    <p>父组件上的显示值：{{text}}</p>
+    <demo-child v-model:text="text" />
   </div>
 </template>
  
 <script lang='ts'>
-import { defineComponent, provide, ref, reactive,getCurrentInstance,  } from "vue";
+import { defineComponent, provide, ref, reactive,getCurrentInstance, } from "vue";
 import demoChild from './demochild.vue'
 interface Value{
     value:string|boolean|number
@@ -51,12 +56,13 @@ export default defineComponent({
     const globalProperty :any=getCurrentInstance()
     console.log(globalProperty.appContext.config.globalProperties,'全局信息')
     const provideData:Value=reactive<Value>({value:'这是接收值'})
+    const text = ref<string>('')
     //将响应式参数传入provide中 完成注入响应式
     provide('provideData',provideData)
     var changeProvideData=function():void{
         provideData.value='传递值发生改变，响应式传入child组件中'
     }
-    return {changeProvideData};
+    return {changeProvideData,text};
   },
 });
 </script>
--- src/components/demochild.vue
+++ src/components/demochild.vue
@@ -1,27 +1,36 @@
 <template>
   <div>
-      <slot name="title"></slot>
+      <!-- <slot name="title"></slot>
       <slot></slot>
       <slot name="foot" :footerMsg="footerMsg"></slot>
-      <div>这是接收的注入的值： {{provideData.value}}</div>
+      <div>这是接收的注入的值： {{provideData.value}}</div> -->
+      <input type="text" v-model="textOfChild">
   </div>
 </template>
  
 <script lang='ts'>
-import { defineComponent, ref, inject,toRefs,unref } from "vue";
+import { defineComponent, ref, inject,toRefs,unref, watchEffect, } from "vue";
 
 export default defineComponent({
   name: "demoChild",
-  props:['msg'],
+  props:['msg','text'],
   setup(props,ctx) {
       console.log(props.msg,unref(toRefs(ctx.attrs).class))
       const title= ref('这是title')
-      const footerMsg= ref('这是footer')
+      const textOfChild=ref<string>('')
+      watchEffect(()=>{
+        ctx.emit('update:text',textOfChild.value)
+      })
+      const footerMsg = ref<string>('这是footer')
       const provideData=inject('provideData')
-    return {title,footerMsg,provideData};
+    return {title,footerMsg,provideData,textOfChild};
   },
 });
 </script>
  
 <style scoped lang = "less">
+input{
+  margin-top: 10px;
+  padding: 5px;
+}
 </style>
