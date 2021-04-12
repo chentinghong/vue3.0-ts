e8e61187959c33e0191ca221fa91613b6aefa6ed
thchen <thchen@hansyinfo.com>
2021-04-12 16:22:41 +0800

    测试插槽setup函数的一些用法

---
+++ src/components/demo.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <p>插槽测试 #代替v-slot写法</p>
+    <demo-child>
+        <template #title>
+            <div>
+                title插槽
+            </div>
+        </template>
+    </demo-child>
+    <demo-child>
+        <template #default>
+            <div>
+                默认插槽
+            </div>
+        </template>
+    </demo-child>
+    <demo-child>
+        <template v-slot:foot="msg">
+            <div>
+                {{msg.footerMsg}}
+            </div>
+        </template>
+    </demo-child>
+    <demo-child>
+        <template #foot="msg">
+            <div>
+                {{msg.footerMsg}}
+            </div>
+        </template>
+    </demo-child>
+    <br>
+    <p>测试provide</p>
+    <demo-child :msg="{msg:'测试传入的参数'}" class="demo" />
+    <button @click="changeProvideData">改变组件中的传递值</button>
+  </div>
+</template>
+ 
+<script lang='ts'>
+import { defineComponent, provide, ref, reactive,getCurrentInstance,  } from "vue";
+import demoChild from './demochild.vue'
+interface Value{
+    value:string|boolean|number
+}
+export default defineComponent({
+  name: "demo",
+  components:{
+      demoChild
+  },
+  setup(props,ctx) {
+    const globalProperty :any=getCurrentInstance()
+    console.log(globalProperty.appContext.config.globalProperties,'全局信息')
+    const provideData:Value=reactive<Value>({value:'这是接收值'})
+    //将响应式参数传入provide中 完成注入响应式
+    provide('provideData',provideData)
+    var changeProvideData=function():void{
+        provideData.value='传递值发生改变，响应式传入child组件中'
+    }
+    return {changeProvideData};
+  },
+});
+</script>
+ 
+<style scoped lang = "less">
+</style>
---
+++ src/components/demochild.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <slot name="title"></slot>
+      <slot></slot>
+      <slot name="foot" :footerMsg="footerMsg"></slot>
+      <div>这是接收的注入的值： {{provideData.value}}</div>
+  </div>
+</template>
+ 
+<script lang='ts'>
+import { defineComponent, ref, inject,toRefs,unref } from "vue";
+
+export default defineComponent({
+  name: "demoChild",
+  props:['msg'],
+  setup(props,ctx) {
+      console.log(props.msg,unref(toRefs(ctx.attrs).class))
+      const title= ref('这是title')
+      const footerMsg= ref('这是footer')
+      const provideData=inject('provideData')
+    return {title,footerMsg,provideData};
+  },
+});
+</script>
+ 
+<style scoped lang = "less">
+</style>
